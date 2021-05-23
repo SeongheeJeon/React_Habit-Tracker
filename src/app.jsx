2d7bccddc4c4ebbbef7ce9af0b527b6ba0312c4c
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-05-24 02:53:07 +0900

    ~4.8 lecture

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import "./app.css";
+import { Component } from "react";
+
+import Habits from "./components/habits";
+
+class App extends Component {
+  render() {
+    return <Habits />;
+  }
+}
+
+export default App;
